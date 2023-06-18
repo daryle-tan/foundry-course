e288ab28875bc8705cf05e9246d7d2e7f94176cd
daryle <daryle.tan@gmail.com>
2023-06-18 14:34:25 -0500

    4:10

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -13,13 +13,22 @@ contract SimpleStorage {
     Person[] public friends;
     // Person public myFriend = Person({favoriteNumber: 7, name: "Tan"});
 
-    function store(uint256 _favoriteNumber) public {
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    function store(uint256 _favoriteNumber) public virtual {
         myFavoriteNumber = _favoriteNumber;
     }
     // view(read), pure no gas spent
     function retrieve() public view returns(uint256){
         return myFavoriteNumber;
     }
-
-
+    // calldata = temporary variable that can't be altered
+    // memory is temporary variable that CAN be altered
+    // storage is permanent variable that can be modified
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        friends.push(Person(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
 }
+
+// struct, mapping, or array must be given a memory variable
